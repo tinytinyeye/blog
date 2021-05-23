2f5e182f7693ac7a96f44b9da66a0b695a8eb05a
frankiewong <frankie.huang95@gmail.com>
2021-05-22 17:08:28 -0700

    chore: setup mdx

---
+++ components/Button.tsx
@@ -0,0 +1,18 @@
+const Button = ({ children }) => {
+    return (
+        <button
+            style={{
+                borderRadius: '3px',
+                border: '1px solid black',
+                color: 'black',
+                padding: '0.5em 1em',
+                cursor: 'pointer',
+                fontSize: '1.1em',
+            }}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default Button;
