d35cc07395f1ece23b6f89a3754ceb0a249a8142
frankiewong <frankie.huang95@gmail.com>
2022-02-02 20:43:16 -0800

    chore: slight style fix for definition section

--- components/WordleSolver/Suggestions.tsx
+++ components/WordleSolver/Suggestions.tsx
@@ -36,6 +36,7 @@ const Definition = forwardRef<HTMLDivElement, { word: string }>(
                         position: 'sticky',
                         top: 0,
                         zIndex: 1,
+                        width: '100%'
                     }}
                     ref={ref}
                 >
